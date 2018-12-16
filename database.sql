163f70c9916ec84dab6b7d46730d2925dc6f8638
Ryan Mundy <ryanmundy34@gmail.com>
2018-12-16 10:23:37 -0600

    adds dummy data to SQL file for testing

--- database.sql
+++ database.sql
@@ -25,3 +25,20 @@ VALUES
     ('SQL'),
     ('Redux'),
     ('HTML');
+
+    --Dummy data for testing
+
+    INSERT INTO projects
+    ("name", "description", "website", "github", "date_completed", "tag_id", "thumbnail")
+VALUES
+    ('FeedBack Portal', 'An application that allows users to collect feedback from others', 'https://nameless-dusk-53745.herokuapp.com/#/', 'https://github.com/ryanmundy/weekend-challenge-5-feedback', '2018-12-10', 1, 'images/Feedback.png');
+
+INSERT INTO projects
+    ("name", "description", "website", "github", "date_completed", "tag_id", "thumbnail")
+VALUES
+    ('To Do List', 'An application that allows users to manage tasks', 'https://afternoon-woodland-10344.herokuapp.com/', 'https://github.com/ryanmundy/vega-todo-app-weekend3', '2018-11-26', 2, 'images/ToDo.png');
+
+INSERT INTO projects
+    ("name", "description", "github", "date_completed", "tag_id", "thumbnail")
+VALUES
+    ('Gallery', 'An application that displays a photo gallery', 'https://github.com/ryanmundy/weekend-4-gallery', '2018-12-03', 1, 'images/Gallery.png');
